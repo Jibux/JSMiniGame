bb814e0bd12c1b5897867316c2c491dce8b35c07
Jean-Baptiste Hugon <jeanb.hugon@gmail.com>
2013-06-29 16:22:20 +0200

    delete test classes

--- minigame/test_classes.js
+++
@@ -1,5 +0,0 @@
-var character = CharacterHelper.newCharacter();
-
-console.log(CharacterHelper.getCurrentLife(character));
-CharacterHelper.setCurrentLife(character,50);
-console.log(CharacterHelper.getCurrentLife(character));
