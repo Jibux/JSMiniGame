8e1010e165cf6637625927fd883a9e76f0293aa1
Jean-Baptiste Hugon <jeanb.hugon@gmail.com>
2013-07-04 23:28:23 +0200

    add usefull scripts

---
+++ scripts/git_pull_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+branchs=`git branch | awk '{print $NF}'`
+current=`git branch | grep '*' | awk '{print $NF}'`
+
+echo "git fetch all..."
+git fetch --all
+
+echo "Update branchs..."
+for branch in `echo -e $branchs`; do
+	git checkout "$branch"      || exit 1
+	git rebase "origin/$branch" || exit 1
+done
+
+echo "Return to original branch '$current'"
+git checkout $current
+
