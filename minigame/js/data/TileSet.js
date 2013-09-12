99d4605d779ff2c45186d3078da0cbe747430213
HermineF <nicolas.declercq@hotmail.fr>
2013-09-12 08:24:32 +0200

    ajout canvas

---
+++ minigame/js/data/TileSet.js
@@ -0,0 +1,4 @@
+var TileSet={
+	"grass":{"img":function(){var src = "resources/images/grass.png"; var img=new Image();img.src=src;return img;}},
+	"water":{"img":function(){var src = "resources/images/water.gif"; var img=new Image();img.src=src;return img;}},
+}
